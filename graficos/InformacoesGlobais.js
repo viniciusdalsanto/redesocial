3385c71c78acd300fd6935cfff8bb9690322f6ba
viniciusdalsanto <vinicius.dal.santo@escola.pr.gov.br>
2024-10-16 11:02:47 -0300

    .

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -1,17 +1,17 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39;
 
-async function visualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    console.log(dados);
-    const paragrafos = document.createElement('p')
-    paragrafos.classList.add('graficos-contaier__texto')
+async function vizualizarInformacoesGlobais() {
+const res = await fetch(url)
+const dados = await res.json()
+console.log(dados);
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
 
-    paragrafos.innerHTML - `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em algumas rede social e passam em media <span>${dados.tempo_medio}</span> horas conectadas.`
-    console.log(paragrafo)
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
 
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafos)
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
 }
 
-visualizarInformacoesGlobais()
+vizualizarInformacoesGlobais()
