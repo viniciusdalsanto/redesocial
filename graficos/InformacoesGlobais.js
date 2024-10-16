3d2f269e71924d06de235b67f3a30287acb41105
viniciusdalsanto <vinicius.dal.santo@escola.pr.gov.br>
2024-10-16 10:04:24 -0300

    .

---
+++ graficos/InformacoesGlobais.js
@@ -0,0 +1,18 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafos = document.createElement('p')
+    paragrafos.classList.add('graficos-contaier__texto')
+
+    paragrafos.innerHTML - `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em algumas rede social e passam em media <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+
+}
+
+visualizarInformacoesGlobais()
