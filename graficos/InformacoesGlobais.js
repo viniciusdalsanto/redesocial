71247f95f8ff7a6fd609e742c363cbf0da282829
viniciusdalsanto <vinicius.dal.santo@escola.pr.gov.br>
2024-10-16 10:43:01 -0300

    .

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -12,7 +12,6 @@ async function visualizarInformacoesGlobais() {
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-
 }
 
 visualizarInformacoesGlobais()
